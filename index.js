b57a599bf3f25b7411cef7b2defda5d4ac6e0de2
Amy Zhou <aymeighz@gmail.com>
2018-07-17 16:27:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,15 +10,27 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
-}
+  let itemObject = {itemName: [item], itemPrice: Math.floor(Math.random()*100)};
+  cart.push(itemObject);
+  return [item] + " has been added to your cart.";  
+  }
 
 function viewCart() {
-  // write your code here
+  var contents = [];
+  let itemPrice = Math.floor(Math.random()*100);
+  let i = 0; 
+  while (i < cart.length) {
+    contents.push(`you have ${i} at ${itemPrice},`);
+  }
+  if (cart.length === 0) {
+    return `Your shopping cart is empty.`;
+} else {
+  return `In your cart, ${contents} and ${i} at $${ itemPrice}.`; 
+}
 }
 
 function total() {
-  // write your code here
+  
 }
 
 function removeFromCart(item) {
